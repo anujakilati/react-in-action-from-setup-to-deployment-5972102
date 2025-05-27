f39e4b70991d28588230c41d8d0a5aa18212ae02
Ananya Jakilati <ajakilat@ucsc.edu>
2025-05-27 15:52:49 -0700

    changes from ahead

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,7 +2,7 @@ function Nav() {
   return (
     <nav className="container">
       <ul>
-        <li><a href="#"><img stlye={{ height: '50px' }} src="images/logo_bug_stargazers.svg" alt="Stargazers Logo" /></a></li>
+        <li><a href="#"><img style={{ height: '50px' }} src="images/logo_bug_stargazers.svg" alt="Stargazers Logo" /></a></li>
         <li><strong>Stargazers</strong></li>
       </ul>
     </nav>
